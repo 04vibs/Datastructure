5068775c08ad547b2263a6d6a56489316a1cae5b
04vibs <04vibs@gmail.com>
2018-02-20 16:53:31 +0530

    Max circular sum

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxCircularSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testcases = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            int Max = array[0];
+            int MaxRgtSum;
+
+            for (int j = 0; j < n; j++)
+            {
+                MaxRgtSum = array[j];
+                for (int k = (j+1)%n; k != (j+n) % n; k=(k+1)%n )
+                {
+                    // MaxRgtSum = Math.Max(MaxRgtSum , array[j]+ MaxRgtSum);
+                    MaxRgtSum += array[k];
+                    if (Max < MaxRgtSum)
+                    {
+                        Max = MaxRgtSum;
+                    }
+                }
+            }
+           
+            Console.WriteLine(Max);
+            Console.ReadLine();
+        }
+    }
+}
